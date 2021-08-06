0df7613a5fa1a8cc53d1cc3ea008379dd23b0c70
Marko Vukosav <marko.vukosav@rimac-automobili.com>
2021-08-06 15:18:06 +0200

    working recyclerView

--- app/src/main/java/com/example/foodapp_demo/MainActivity.kt
+++ app/src/main/java/com/example/foodapp_demo/MainActivity.kt
@@ -2,31 +2,34 @@ package com.example.foodapp_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var recyclerView: RecyclerView
-
-
-    var menu1 = dataClass("Meni 1", "Piletina i riža", 1)
-    var menu2 = dataClass("Meni 2", "janjetina i riža", 2)
-    var menu3 = dataClass("Meni 3", "kruh i riža", 3)
-    var menu4 = dataClass("Meni 4", "narezak i riža", 4)
-    var menu5 = dataClass("Meni 5", "wok", 5)
+ private   lateinit var recyclerView: RecyclerView
+ private lateinit var adapter: MyAdapter
 
 
 
+     var titles = arrayListOf("Meni 1", "Meni 2","Meni 3","Meni 4","Meni 5",)
+
+
+//    lateinit var s1: String
+//    lateinit var s2: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var arraylistOfTitles = arrayListOf(menu1.Title,menu2.Title,menu3.Title,menu4.Title,menu5.Title)
-        var arraylistOfDescription = arrayListOf(menu1.Description,menu2.Description,menu3.Description,menu4.Description,menu5.Description)
-        var arraylistOfImages = arrayListOf(menu1.Image,menu2.Image,menu3.Image,menu4.Image,menu5.Image)
+
+
 
         recyclerView = findViewById(R.id.recyclerView)
+        adapter = MyAdapter(this, titles)
+        
+       recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/foodapp_demo/MyAdapter.kt
+++ app/src/main/java/com/example/foodapp_demo/MyAdapter.kt
@@ -1,18 +1,16 @@
 package com.example.foodapp_demo
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.*
-import kotlin.io.path.createTempDirectory
-import kotlin.properties.Delegates
 
-class MyAdapter(private val title: Array<String>,private val description: Array<String>,private val img: Array<Int> ) : Adapter<MyAdapter.MyViewHolder>() {
+open class MyAdapter(private val context: Context, private val title: ArrayList<String>) : Adapter<MyAdapter.MyViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
@@ -22,25 +20,19 @@ class MyAdapter(private val title: Array<String>,private val description: Array<
 
     override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
        holder.textViewT.text = title[position]
-       holder.textViewD.text = description[position]
-       holder.imageView.setImageResource() = img[position]
+
+        Log.i("{$position}","Marko ")
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return 5
     }
 
-    class MyViewHolder(itemView: View) : ViewHolder(itemView) {
-        val textViewT: TextView
-        val textViewD: TextView
-        val imageView: ImageView
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textViewT: TextView = itemView.findViewById(R.id.title1)
 
-        init {
-            // Define click listener for the ViewHolder's View.
-            textViewT = itemView.findViewById(R.id.titleMenu)
-            textViewD = itemView.findViewById(R.id.description)
-            imageView = itemView.findViewById(R.id.foodImage)
-        }
+
+
     }
 }
 
